ce21a7874afc0909537aa3d83c059883fd367cbe
Kim Su Chol <nikita.kim1993@gmail.com>
2023-02-04 12:02:49 +0900

    Task done

--- src/main/java/web/controller/HelloController.java
+++ src/main/java/web/controller/HelloController.java
@@ -1,15 +1,20 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RequestParam;
+import web.service.CarsService;
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class HelloController {
+
+	@Autowired
+	private CarsService cars;
 
 	@GetMapping(value = "/")
 	public String printWelcome(ModelMap model) {
@@ -19,6 +24,12 @@ public class HelloController {
 		messages.add("5.2.0 version by sep'19 ");
 		model.addAttribute("messages", messages);
 		return "index";
+	}
+
+	@GetMapping(value = "/cars")
+	public String getCars(@RequestParam(value = "count", defaultValue = "5") Integer value, Model model) {
+		model.addAttribute("listOfCars", cars.getCars(value));
+		return "cars";
 	}
 	
 }
